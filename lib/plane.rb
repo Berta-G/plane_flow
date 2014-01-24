677d2bb60ad717a5b4ec2120d3d6bf722094417c
Berta-G <berta.gutierrez@gmail.com>
2014-01-24 13:19:27 +0000

    Plane class and rspect (tests for stormy weather not ok yet)

--- lib/plane.rb
+++ lib/plane.rb
@@ -1,11 +1,34 @@
 
 class Plane
 
-	def initialize(status = flying)
+	def initialize(status = :flying)
 		@status = status
 	end
 
 	attr_accessor :status
-	
+
+	def status
+		@status
+	end
+
+	def take_off(airport)
+		airport.take_off(self)
+		plane_is_flying
+	end
+
+	def land(airport)
+		airport.land(self)
+		plane_has_landed
+	end
+
+	def plane_is_flying
+		@status = :flying
+	end
+
+	def plane_has_landed
+		@status = :landed
+	end
+
+
 
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -3,9 +3,30 @@ require_relative '../lib/plane'
 
 describe Plane do
 	let(:plane) {Plane.new}
+	let(:airport) {Airport.new}
 
 	it "has a flying status when created" do
-		expect(plane.status).to be(:flying)
+		expect(plane.status).to eq(:flying)
 	end
+
+	it "can land on an airport" do
+		plane.land(airport)
+		expect(plane.status).to eq(:landed)
+	end
+
+	it "can take off from an airport" do
+		plane.status = :landed
+		plane.take_off(airport)
+		expect(plane.status).to eq(:flying)
+	end
+
+	it "cannot land if stormy weather" do
+		expect {plane.land(airport)}.to raise_error
+	end
+
+	it "cannot take off if stormy weather" do
+		expect {plane.take_off(airport)}.to raise_error
+	end
+
 
 end
